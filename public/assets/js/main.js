8d34be0b52e4485c6f78804558fa4269dbcfe679
Sunny Batabyal <batabyalsunny@gmail.com>
2019-04-20 01:01:23 +0530

    Edit modal filled with data

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,6 +1,6 @@
-$(document).ready(function () {
+const API_BASE = "http://localhost:8080/api/v1"
 
-    const API_BASE = "http://localhost:8080/api/v1"
+$(document).ready(function () {
 
     $('#sidebarCollapse').on('click', function () {
         $('#sidebar').toggleClass('active')
@@ -19,8 +19,8 @@ $(document).ready(function () {
                         'lastName': json[i].lastName,
                         'email': json[i].email,
                         'phoneNumber': json[i].phoneNumber,
-                        'addressId': json[i].address.id,
-                        'address': json[i].address.city.name,
+                        'address': json[i].address,
+                        'cityName': json[i].address.city.name,
                         'companyId': json[i].company.id,
                         'company': json[i].company.name,
                         'designationId': json[i].designation.id,
@@ -34,7 +34,7 @@ $(document).ready(function () {
             {'data': 'firstName'},
             {'data': 'email'},
             {'data': 'phoneNumber'},
-            {'data': 'address'},
+            {'data': 'cityName'},
             {'data': 'company'},
             {'data': 'designation'},
             {
@@ -42,8 +42,13 @@ $(document).ready(function () {
                 'orderable': false,
                 'mRender': function ( data, type, row ) {
                     return '<a href="#" class="edit-button" data-toggle="modal" data-target="#editModal" data-id="'
-                    + row.id +'" data-address-id="'+ row.addressId +'" data-company-id="'+ row.companyId
-                    +'" data-designation-id="'+ row.designationId +'"><i class="fas fa-pencil"></i></a>';
+                    + row.id +'" data-address-id="'+ row.address.id +'" data-company-id="'+ row.companyId
+                    +'" data-designation-id="'+ row.designationId +'" data-city-id="'+row.address.city.id
+                    +'" data-state-id="'+ row.address.city.state.id +'" data-country-id="'+ row.address.city.state.country.id
+                    +'" data-first-name="'+ row.firstName +'" data-last-name="'+ row.lastName
+                    +'" data-email="'+ row.email +'" data-phone-number="'+ row.phoneNumber
+                    +'" data-address-line-1="'+ row.address.addressLine1 +'" data-address-line-2="'+ row.address.addressLine2
+                    +'"><i class="fas fa-pencil"></i></a>';
                 }
             },
             {
@@ -59,67 +64,123 @@ $(document).ready(function () {
     $('table').on('click', '.edit-button', function () {
         var id = $(this).data('id')
         var addressId = $(this).data('address-id')
+        var cityId = $(this).data('city-id')
+        var stateId = $(this).data('state-id')
+        var countryId = $(this).data('country-id')
         var companyId = $(this).data('company-id')
         var designationId = $(this).data('designation-id')
+        
+        // Fill up the modal form.
+        $('#firstName').val($(this).data('first-name'))
+        $('#lastName').val($(this).data('last-name'))
+        $('#email').val($(this).data('email'))
+        $('#phoneNumber').val($(this).data('phone-number'))
+        $('#addressLine1').val($(this).data('address-line-1'))
+        $('#addressLine2').val($(this).data('address-line-2'))
 
-        // ajax call.
-        $.ajax({
-            type: "GET",
-            url: API_BASE + "/employees/" + id,
-            success: function (res) {
-                // Fill up the modal.
-                $('#firstName').val(res.firstName)
-                $('#lastName').val(res.lastName)
-                $('#email').val(res.email)
-                $('#phoneNumber').val(res.phoneNumber)
-                $('#company').append('<option selected value="'+ res.company.id +'">'+ res.company.name +'</option>')
-                $('#designation').append('<option selected value="'+ res.designation.id +'">'+ res.designation.description +'</option>')
-
-                $('#country').append('<option selected value="'+ res.address.city.state.country.id +'">'+ res.address.city.state.country.name +'</option>')
-                $('#state').append('<option selected value="'+ res.address.city.state.id +'">'+ res.address.city.state.name +'</option>')
-                $('#city').append('<option selected value="'+ res.address.city.id +'">'+ res.address.city.name +'</option>')
-
-                $('#addressLine1').val(res.address.addressLine1)
-                $('#addressLine2').val(res.address.addressLine2)
-            }
-        })
-
-        // Company call.
-        $.ajax({
-            type: "GET",
-            url: API_BASE + "/companies",
-            success: function (res) {
-                res.forEach(function (company) {
-                    if (companyId !== company.id)
-                        $('#company').append('<option value="'+ company.id +'">'+ company.name +'</option>')
-                })
-            }
-        })
-
-        // Designation call.
-        $.ajax({
-            type: "GET",
-            url: API_BASE + "/designations",
-            success: function (res) {
-                res.forEach(function (designation) {
-                    if (designationId !== designation.id)
-                        $('#designation').append('<option value="'+ designation.id +'">'+ designation.description +'</option>')
-                })
-            }
-        })
-
-        // Country call.
-        $.ajax({
-            type: "GET",
-            url: API_BASE + "/countries",
-            success: function (res) {
-                res.forEach(function (country) {
-                    // if (companyId !== company.id)
-                        $('#country').append('<option value="'+ country.id +'">'+ country.name +'</option>')
-                })
-            }
-        })
+        fillCompanies(companyId)
+        fillDesignations(designationId)
+        fillCountries(countryId)
+        fillStates(countryId, stateId)
+        fillCities(stateId, cityId)
 
     })
 
+    $('#editModal').on('change', 'select#country', function () {
+        // Get the country ID.
+        var countryId = $(this).val()
+        fillStates(countryId)
+    })
+
+    $('#editModal').on('change', 'select#state', function () {
+        // Get the state ID.
+        var stateId = $(this).val()
+        fillCities(stateId)
+    })
+
 })
+
+var fillCompanies = function (companyId) {
+    // Clear old.
+    $('#company').empty()
+
+    // Company call.
+    $.ajax({
+        type: "GET",
+        url: API_BASE + "/companies",
+        success: function (res) {
+            res.forEach(function (company) {
+                var selected = (companyId === company.id) ? 'selected' : '';
+                $('#company').append('<option '+ selected +' value="'+ company.id +'">'+ company.name +'</option>')
+            })
+        }
+    })
+}
+
+var fillDesignations = function (designationId) {
+    // Clear old.
+    $('#designation').empty()
+
+    // Designation call.
+    $.ajax({
+        type: "GET",
+        url: API_BASE + "/designations",
+        success: function (res) {
+            res.forEach(function (designation) {
+                var selected = (designationId === designation.id) ? 'selected' : '';
+                $('#designation').append('<option '+ selected +' value="'+ designation.id +'">'+ designation.description +'</option>')
+            })
+        }
+    })
+}
+
+var fillCountries = function (countryId) {
+    // Clear old.
+    $('#country').empty()
+
+    // Country call.
+    $.ajax({
+        type: "GET",
+        url: API_BASE + "/countries",
+        success: function (res) {
+            res.forEach(function (country) {
+                var selected = (countryId === country.id) ? 'selected' : '';
+                $('#country').append('<option '+ selected +' value="'+ country.id +'">'+ country.name +'</option>')
+            })
+        }
+    })
+}
+
+var fillStates = function (countryId, clearPrevious, stateId) {
+    // Clear the select box.
+    $('#state').empty()
+
+    // State call.
+    $.ajax({
+        type: "GET",
+        url: API_BASE +"/countries/"+ countryId +"/states",
+        success: function (res) {
+            res.forEach(function (state) {
+                var selected = (stateId === state.id) ? 'selected' : '';
+                $('#state').append('<option '+ selected +' value="'+ state.id +'">'+ state.name +'</option>')
+            })
+        }
+    })
+}
+
+var fillCities = function (stateId, clearPrevious, cityId) {
+    // Clear the select box.
+    $('#city').empty()
+
+    // City call.
+    $.ajax({
+        type: "GET",
+        url: API_BASE +"/states/"+ stateId +"/cities",
+        success: function (res) {
+            res.forEach(function (city) {
+                var selected = (cityId === city.id) ? 'selected' : '';
+                $('#city').append('<option '+ selected +' value="'+ city.id +'">'+ city.name +'</option>')
+            })
+        }
+    })
+}
